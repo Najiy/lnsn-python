9ddd7af3ae86db625e06d88363da8471cb6ce049
najiy <najiy.wa@googlemail.com>
2023-06-02 01:41:24 +0100

    .

--- Data Preprocessors/dataset_online_MIT_subject_preprocess.py
+++ Data Preprocessors/dataset_online_MIT_subject_preprocess.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
 import pprint as pp
+import json
 from datetime import datetime as datetime
 
-def read_sensors(path='../dataset online/MIT/subject1/sensors.csv'):
+folder = 'subject1'
+
+def read_sensors(path=f'../dataset online/MIT/{folder}/sensors.csv'):
     sensors = pd.read_csv(path)
     return sensors
 
-def read_activities(folder='subject1'):
+def read_activities():
     activities_fp = open(f'../dataset online/MIT/{folder}/activities_data.csv', 'r+').readlines()
     sensors_fp = open(f'../dataset online/MIT/{folder}/sensors.csv', 'r+').readlines()
 
@@ -23,10 +26,8 @@ def read_activities(folder='subject1'):
 
     csv_header = sensors
     csv_header.insert(0, "time")
-    # print(csv_header)
-    # input()
 
-    # activities_dict = {}
+    activities_dict = {}
     timeline = {}
 
     mintime = -1
@@ -43,6 +44,7 @@ def read_activities(folder='subject1'):
         starttimes = starttimes.replace('\n','').split(',')
         endtimes = endtimes.replace('\n','').split(',')
 
+        sequence = []
         for i, t in enumerate(starttimes):
             try:
                 start = int(datetime.strptime(f"{header[1]} {starttimes[i]}", "%m/%d/%Y %H:%M:%S").strftime('%s'))
@@ -60,12 +62,19 @@ def read_activities(folder='subject1'):
 
                 timeline[start].append((sensorid[i], 1))
                 timeline[end].append((sensorid[i], 0))
+                sequence.append([sensorid[i], start, end])
             except:
                 pass
+        
         counter += 5
-        # input()
 
-    # pp.pprint(timeline)
+        if header[0] not in activities_dict:
+            activities_dict[header[0]] = []
+
+        sequence.sort(key=lambda x: x[1])
+        
+        activities_dict[header[0]].append(sequence)
+
     csv_timeline = []
 
     for i in range(mintime, maxtime):
@@ -74,18 +83,8 @@ def read_activities(folder='subject1'):
             entry[0] = str(i)
             for stup in timeline[i]:
                 entry[sensors.index(stup[0])] = str(stup[1])
-                # print(stup)
-                # input()
-                # entry[] =
-            # print(entry.count(''), entry)
-            # if entry.count('') is not 69:
-            #     input()
-            # print(sensors, len(sensors))
-            # print(entry, len(entry))
-            # input()
+            
             csv_timeline.append(','.join(entry)+',\n')
-    # print(sensors)
-    # input()
     csv_timeline.insert(0, ','.join(sensors)+',\n')
 
     csv_meta = []
@@ -93,9 +92,11 @@ def read_activities(folder='subject1'):
     for i in sensors:
         csv_meta.append(f'{i},,,,,,,,,0,1,,,\n')
 
-    return csv_timeline, csv_meta
+    return csv_timeline, csv_meta, activities_dict
 
-activities, meta = read_activities()
-# pp.pprint(meta)
-open('../dataset online/MIT/subject1/subject1.csv','w+').writelines(activities)
-open('../dataset online/MIT/subject1/subject1_meta.csv','w+').writelines(meta)
+activities, meta, activities_dict = read_activities()
+
+open(f'../dataset online/MIT/{folder}/{folder}.csv','w+').writelines(activities)
+open(f'../dataset online/MIT/{folder}/{folder}_meta.csv','w+').writelines(meta)
+with open(f'../dataset online/MIT/{folder}/{folder}_act_dict.json', 'w+', encoding='utf-8') as f:
+    json.dump(activities_dict, f, ensure_ascii=False, indent=4)
